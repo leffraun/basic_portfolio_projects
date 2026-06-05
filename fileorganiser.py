6e8124d72012ea9b62170dc98737b77d60305510
leffraun <143820813+leffraun@users.noreply.github.com>
2026-06-05 14:54:29 +0530

    Organize file creation and subfolder management
    Refactor file creation and organization logic into functions.

--- fileorganiser.py
+++ fileorganiser.py
@@ -1,34 +1,42 @@
 """
 create files and organise them into folders based on their extensions
 """
-import os
-from pathlib import Path
+#first function is to create a file
+#next one is to create subfolders and arranges them
+#and a third displays all this to console.
 import shutil
-#create a folder
-folder= Path("TrialRun")
-folder.mkdir(exist_ok=True)
+from pathlib import Path
+
+folder=Path("Another")
 files=[]
-folders=[]
-n=int(input("how many files do you want to add:"))
-for i in range(n):
-    file=input(f"input {i+1}:")
-    files.append(Path(file))
-for file in files:
-    file_path=folder/file
-    file_path.touch()
-
-for file in folder.iterdir():
-    if file.is_file():
-        ext=file.suffix[1:] if file.suffix else "others"
-        if ext not in folders:
-            subfolder_path=folder/ext
-            subfolder_path.mkdir(exist_ok=True)
-            folders.append(ext)
-        shutil.move(str(file),str(subfolder_path/file.name))
+subfolder=[]
+def create_file(folder,files):
+    folder.mkdir(exist_ok=True) #creating the file
+    print("file is set!")
+    n=int(input("enter number of files you want to add to:"))
+    for i in range(n):
+        file=input("enter the name of the file you want to add:")
         files.append(file)
+        file_path=folder/file
+        file_path.touch() #create file in that folder
 
 
-print(folders)
-for item in folder.iterdir():
-    print(item)
+def create_subfolder(folder,subfolder):
+    for file in folder.iterdir():
+        ext=file.suffix[1:] if file.suffix else "others"
+        if ext not in subfolder:
+            subfolder.append(ext)
+        subfolder_path=folder/ext
+        subfolder_path.mkdir(exist_ok=True)
+        shutil.move(str(file), str(subfolder_path/file.name))
+
+
+def displayfiles(folder):
+    for file in folder.iterdir():
+        print(file)
+
+
+create_file(folder,files)
+create_subfolder(folder,subfolder)
+displayfiles(folder)
 
