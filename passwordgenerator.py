e1a47ebad9ac1cd1eb076e86411ce046b49b565d
leffraun <143820813+leffraun@users.noreply.github.com>
2026-06-07 21:38:31 +0530

    Enhance password creation with digits and punctuation
    Refactor password generation to include digits and punctuation.

--- passwordgenerator.py
+++ passwordgenerator.py
@@ -1,22 +1,29 @@
 """
 create a 12-15 digit password with as many punctuations as you can
 """
+"""
+create a 12-15 digit password with as many punctuations as you can
+"""
 import random
 import string
 num=random.randint(12,15) #randomly decide number of letters in password
 def create_password(num):
 
     password=""
-    specialCharacters=["!","?","%","$","#","&"]
-    for i in range(num):
-        choice=random.randint(1,2)
-        match choice:
-            case 1:
-                specialChoice=random.choice(specialCharacters)
-                password+=specialChoice
-            case 2:
-                password+=random.choice(string.ascii_letters)
-    return password
+    try:
+        specialCharacters=["!","?","%","$","#","&"]
+        for i in range(num):
+            choice=random.randint(1,3)
+            match choice:
+                case 1:
+                    password+=random.choice(string.punctuation)
+                case 2:
+                    password+=random.choice(string.ascii_letters)
+                case 3:
+                    password+=random.choice(string.digits)
+        return password
+    except:
+        print("error in password creation")
 
 print(create_password(num))
 
